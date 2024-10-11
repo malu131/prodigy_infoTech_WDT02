bc3127cf3f0702d0cd80b6a523f0a5ca3468c63d
malu131 <107981827+malu131@users.noreply.github.com>
2024-10-12 01:49:26 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let startTime;
+let updatedTime;
+let difference;
+let tInterval;
+let running = false;
+
+const display = document.getElementById('display');
+
+function startTimer() {
+    if (!running) {
+        startTime = new Date().getTime();
+        tInterval = setInterval(getShowTime, 1);
+        running = true;
+    }
+}
+
+function stopTimer() {
+    clearInterval(tInterval);
+    running = false;
+}
+
+function resetTimer() {
+    clearInterval(tInterval);
+    running = false;
+    display.innerHTML = "00:00:00";
+    difference = 0;
+}
+
+function getShowTime() {
+    updatedTime = new Date().getTime();
+    difference = updatedTime - startTime;
+    let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    display.innerHTML = hours + ":" + minutes + ":" + seconds;
+}
+
+document.getElementById('start').onclick = startTimer;
+document.getElementById('stop').onclick = stopTimer;
+document.getElementById('reset').onclick = resetTimer;
---
+++ views.py
@@ -0,0 +1,9 @@
+
+from django.shortcuts import render
+
+def stopwatch(request):
+    return render(request, 'stopwatch.html')
+
+
+
+# Create your views here.
